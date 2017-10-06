c1b1b8bd1a76b47f3f03a2dc9cb706a2d2cfee17
Almat <Almat231998@gmail.com>
2017-10-06 11:51:36 +0600

    Hello.java файылына өзгеріс енгізілді.

--- .idea/src/kz/almat/Hello.java
+++ .idea/src/kz/almat/Hello.java
@@ -3,6 +3,8 @@ package kz.almat;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello Almat");
+        System.out.println("Hello");
     }
 }
 
